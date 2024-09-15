798a8b6d92076b57707ecea50f4497044efa2d08
Daniel Españadero <developer@daniel-espanadero.com>
2024-09-15 10:24:20 +0200

    Created Domain data

--- backend/src/main/java/com/diveger/backend/domain/People.java
+++ backend/src/main/java/com/diveger/backend/domain/People.java
@@ -1,4 +1,29 @@
 package com.diveger.backend.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class People {
+    private String birthYear;
+    private String eyeColor;
+    private List<Film> films;
+    private String gender;
+    private String hairColor;
+    private String height;
+    private String planetName;
+    private String homeworld;
+    private String mass;
+    private String name;
+    private String skinColor;
+    private String created;
+    private String edited;
+    private List<Specie> species;
+    private List<Starship> starships;
+    private String url;
+    private List<Vehicle> vehicles;
+    private String fastestVehicleDriven;
 }
