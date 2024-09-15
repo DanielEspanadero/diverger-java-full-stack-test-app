688f9dab2b5e46f0f3cea3fff4a82e524e62528a
Daniel Españadero <developer@daniel-espanadero.com>
2024-09-15 10:56:15 +0200

    Mapped CharacterResponseDTO from domain People

--- backend/src/main/java/com/diveger/backend/application/mappers/PeopleMapper.java
+++ backend/src/main/java/com/diveger/backend/application/mappers/PeopleMapper.java
@@ -1,4 +1,31 @@
 package com.diveger.backend.application.mappers;
 
+import com.diveger.backend.application.dto.CharacterResponseDTO;
+import com.diveger.backend.application.dto.FilmResponseDTO;
+import com.diveger.backend.domain.People;
+
+import java.util.List;
+
 public class PeopleMapper {
+
+    public CharacterResponseDTO peopleToCharacterResponse(People people) {
+        if (people == null) {
+            return null;
+        }
+
+        CharacterResponseDTO dto = new CharacterResponseDTO();
+        dto.setName(people.getName());
+        dto.setGender(people.getGender());
+        dto.setPlanetName(people.getPlanetName());
+        dto.setFastestVehicleDriven(people.getFastestVehicleDriven());
+        dto.setBirthYear(people.getBirthYear());
+
+        List<FilmResponseDTO> filmDTOs = people.getFilms().stream()
+                .map(film -> new FilmResponseDTO(film.getTitle(), film.getReleaseDate()))
+                .toList();
+
+        dto.setFilms(filmDTOs);
+        
+        return dto;
+    }
 }
--- backend/src/main/java/com/diveger/backend/application/services/StarWarsService.java
+++ backend/src/main/java/com/diveger/backend/application/services/StarWarsService.java
@@ -1,4 +1,20 @@
 package com.diveger.backend.application.services;
 
-public class StarWarsService {
+import com.diveger.backend.application.dto.CharacterResponseDTO;
+import com.diveger.backend.application.ports.in.PersonInfoUseCase;
+import com.diveger.backend.application.ports.out.PersonInfoPort;
+
+import java.util.List;
+
+public class StarWarsService implements PersonInfoUseCase {
+    private final PersonInfoPort personInfoPort;
+
+    public StarWarsService(PersonInfoPort personInfoPort) {
+        this.personInfoPort = personInfoPort;
+    }
+
+    @Override
+    public List<CharacterResponseDTO> getPersonInfo(String name) {
+        return personInfoPort.getPersonInfo(name);
+    }
 }
