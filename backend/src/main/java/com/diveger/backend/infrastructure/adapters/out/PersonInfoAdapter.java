d2ec8a01b8e6bf8537513f88966692ab1b140c75
Daniel Españadero <developer@daniel-espanadero.com>
2024-09-15 18:27:09 +0200

    Validating that the entered name is not empty or null in PersonInfoAdapter

--- backend/src/main/java/com/diveger/backend/infrastructure/adapters/out/PersonInfoAdapter.java
+++ backend/src/main/java/com/diveger/backend/infrastructure/adapters/out/PersonInfoAdapter.java
@@ -1,6 +1,30 @@
 package com.diveger.backend.infrastructure.adapters.out;
 
-public class PersonInfoAdapter {
+import com.diveger.backend.application.dto.CharacterResponseDTO;
+import com.diveger.backend.application.ports.out.PersonInfoPort;
+import com.diveger.backend.infrastructure.common.exceptions.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class PersonInfoAdapter implements PersonInfoPort {
+
+    @Override
+    public List<CharacterResponseDTO> getPersonInfo(String name) {
+        validateCharacterName(name);
+
+        return null;
+    }
+
+    private void validateCharacterName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            log.error("Character name is null or empty");
+            throw new BadRequestException("The character name must not be empty.");
+        }
+    }
 
     // Obtain person info from the Star Wars API
 
