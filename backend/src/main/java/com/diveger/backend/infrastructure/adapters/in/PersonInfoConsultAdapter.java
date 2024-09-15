8aafc357e736acb9a7501560a4cd83993d450db1
Daniel Españadero <developer@daniel-espanadero.com>
2024-09-15 18:20:14 +0200

    Created exceptions and PersonInfoConsultAdapter

--- backend/src/main/java/com/diveger/backend/infrastructure/adapters/in/PersonInfoConsultAdapter.java
+++ backend/src/main/java/com/diveger/backend/infrastructure/adapters/in/PersonInfoConsultAdapter.java
@@ -1,4 +1,19 @@
 package com.diveger.backend.infrastructure.adapters.in;
 
-public class PersonInfoConsultAdapter {
+import com.diveger.backend.application.dto.CharacterResponseDTO;
+import com.diveger.backend.application.ports.out.PersonInfoPort;
+
+import java.util.List;
+
+public class PersonInfoConsultAdapter implements PersonInfoPort {
+    private final PersonInfoPort personInfoPort;
+
+    public PersonInfoConsultAdapter(PersonInfoPort personInfoPort) {
+        this.personInfoPort = personInfoPort;
+    }
+
+    @Override
+    public List<CharacterResponseDTO> getPersonInfo(String name) {
+        return personInfoPort.getPersonInfo(name);
+    }
 }
--- backend/src/main/java/com/diveger/backend/infrastructure/common/exceptions/BadRequestException.java
+++ backend/src/main/java/com/diveger/backend/infrastructure/common/exceptions/BadRequestException.java
@@ -1,4 +1,10 @@
 package com.diveger.backend.infrastructure.common.exceptions;
 
-public class BadRequestException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class BadRequestException extends ResponseStatusException {
+    public BadRequestException(String reason) {
+        super(HttpStatus.BAD_REQUEST, reason);
+    }
 }
